1a2861c2aa0cc3ca1aeda5a0ebd63c4a698294d3
a676-code <109701008+thisusernameisaparadox@users.noreply.github.com>
2023-03-20 15:14:07 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# van-eck-sequence
+The script `van_eck_sequence.py` generates the [Van Eck sequence](https://www.youtube.com/watch?v=etMJxB-igrc). 
---
+++ van_eck_sequence.py
@@ -0,0 +1,36 @@
+# van_eck_sequence.py
+# Andrew Lounsbury
+# 20/3/23
+# generate the Van Eck sequence; https://www.youtube.com/watch?v=etMJxB-igrc
+
+sequence = [0]
+
+# computes the last time a number appeared in the sequence
+def last_seen(sequence, num):
+    sequence.reverse()
+    pos = 0
+    if len(sequence) == 1 and num == 0:
+        return 0
+    else:
+        curNum = sequence[pos]
+        firstFound = False
+        while curNum != num or not firstFound:
+            pos += 1
+            curNum = sequence[pos]
+            if curNum == num:
+                firstFound = True
+    sequence.reverse()
+    return pos
+        
+
+# generates the sequence up to the n-th number
+def generate(sequence, n):
+    while len(sequence) < n:
+        subsequence = sequence[:len(sequence) - 1]
+        if sequence[len(sequence) - 1] in subsequence:
+            sequence.append(last_seen(sequence, sequence[len(sequence) - 1]))
+        else:
+            sequence.append(0)
+          
+generate(sequence, 10)
+print(sequence)
